a9b3771553e4ffa6fee9d414b90e2f154cb258c6
MinCoffee98vih@gmail.com <isa98vih@gmail.com>
2023-04-18 23:03:54 -0300

    primeira tela card

---
+++ Studyum/resources/views/dashboard.blade.php
@@ -0,0 +1,87 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="{{url('css/templates/css-reset.css')}}" />
+        <link rel="stylesheet" href="{{url('css/templates/header-nav.css')}}" />
+        <link rel="stylesheet" href="{{url('css/templates/footer.css')}}" />
+        <link rel="stylesheet" href="{{url('css/landing-page-about/about.css')}}" />
+        <link rel="stylesheet" href="{{url('css/dashboard/dashboard.css')}}" />
+    <title>Dashboard Painel</title>
+</head>
+<body>
+    <div class="side-menu">
+        <div class="brand-name">
+            <h1>Studyum</h1>
+        </div>
+        <ul>
+            <li><img src="./img/dashboard 24px.png">&nbsp; <span>Dashboard</span></li>
+            <li><img src="./img/book 24px.png">&nbsp; <span>Matérias</span></li>
+            <li><img src="./img/forum 24px.png">&nbsp; <span>Forúm</span></li>
+            <li><img src="./img/lista 24px.png">&nbsp; <span>Lista de Tarefas</span></li>
+            <li></li>
+        </ul>
+    </div>
+    <div class="container">
+        <div class="header">
+            <div class="nav">
+                <div class="search">
+                    <input type="text" placeholder="Search..">
+                    <button type="submit"><img src="./img/search.png" alt=""></button>
+                </div>
+                <div class="user">
+                    <a href="#" class="btn">exemplo</a>
+                    <img src="./img/notificacion 24px.png" alt="">
+                </div>
+                <div class="img-case">
+                    <img src="" alt="">
+                </div>
+            </div>
+        </div>
+        <div class="content">
+            <div class="cards">
+                <div class="card">
+                    <div class="box">
+                        <h1>2194</h1>
+                        <h3>Students</h3>
+                    </div>
+                    <div class="icon-case">
+                        <img src="./img/user 48px.png.png" alt="">
+                    </div>
+                </div>
+                <div class="card">
+                    <div class="box">
+                        <h1>6</h1>
+                        <h3>Schools</h3>
+                    </div>
+                    <div class="icon-case">
+                        <img src="./img/user 48px.png.png" alt="">
+                    </div>
+                </div>
+                <div class="card">
+                    <div class="box">
+                        <h1>5</h1>
+                        <h3>teachers</h3>
+                    </div>
+                    <div class="icon-case">
+                        <img src="./img/user 48px.png.png" alt="">
+                    </div>
+                </div>
+                <div class="card">
+                    <div class="box">
+                        <h1>5000</h1>
+                        <h3>Income</h3>
+                    </div>
+                    <div class="icon-case">
+                        <img src="./img/user 48px.png.png" alt="">
+                    </div>
+                </div>
+                <div class="content-2"></div>
+            </div>
+        </div>
+    </div>
+</body>
+</html> 
