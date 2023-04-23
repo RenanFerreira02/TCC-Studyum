eb977a316cafd4e7c778bd0ae9597bce932866ce
MinCoffee <isa98vih@gmail.com>
2023-04-23 03:21:21 -0300

    dashboard alteração

--- Studyum/resources/views/dashboard.blade.php
+++ Studyum/resources/views/dashboard.blade.php
@@ -13,75 +13,105 @@
     <title>Dashboard Painel</title>
 </head>
 <body>
-    <div class="side-menu">
+<body>
+<div class="side-menu">
         <div class="brand-name">
-            <h1>Studyum</h1>
+            <h1></h1>
         </div>
-        <ul>
-            <li><img src="./img/dashboard 24px.png">&nbsp; <span>Dashboard</span></li>
-            <li><img src="./img/book 24px.png">&nbsp; <span>Matérias</span></li>
-            <li><img src="./img/forum 24px.png">&nbsp; <span>Forúm</span></li>
-            <li><img src="./img/lista 24px.png">&nbsp; <span>Lista de Tarefas</span></li>
-            <li></li>
-        </ul>
-    </div>
-    <div class="container">
-        <div class="header">
-            <div class="nav">
-                <div class="search">
-                    <input type="text" placeholder="Search..">
-                    <button type="submit"><img src="./img/search.png" alt=""></button>
-                </div>
-                <div class="user">
-                    <a href="#" class="btn">exemplo</a>
-                    <img src="./img/notificacion 24px.png" alt="">
-                </div>
-                <div class="img-case">
-                    <img src="" alt="">
-                </div>
-            </div>
+       
+            <ul class="nav-links">
+                <li><img src="{{url('images/dashboard/dashboard.png')}}">&nbsp; <span>Dashboard</span></li>
+                <li><img src="{{url('images/dashboard/book.png')}}">&nbsp; <span>Matérias</span></li>
+                <li><img src="{{url('images/dashboard/forum.png')}}">&nbsp; <span>Forúm</span></li>
+                <li><img src="{{url('images/dashboard/lista.png')}}">&nbsp; <span>Lista de Tarefas</span></li>
+                <li class="logout"><img src="{{url('images/dashboard/logout.png')}}">&nbsp; <span>Desconectar</span></li>
+            </ul>
         </div>
+    </div>  
+    <section class="container">
         <div class="content">
-            <div class="cards">
-                <div class="card">
-                    <div class="box">
-                        <h1>2194</h1>
-                        <h3>Students</h3>
-                    </div>
-                    <div class="icon-case">
-                        <img src="./img/user 48px.png.png" alt="">
-                    </div>
-                </div>
-                <div class="card">
-                    <div class="box">
-                        <h1>6</h1>
-                        <h3>Schools</h3>
-                    </div>
-                    <div class="icon-case">
-                        <img src="./img/user 48px.png.png" alt="">
-                    </div>
-                </div>
-                <div class="card">
-                    <div class="box">
-                        <h1>5</h1>
-                        <h3>teachers</h3>
-                    </div>
-                    <div class="icon-case">
-                        <img src="./img/user 48px.png.png" alt="">
-                    </div>
-                </div>
-                <div class="card">
-                    <div class="box">
-                        <h1>5000</h1>
-                        <h3>Income</h3>
-                    </div>
-                    <div class="icon-case">
-                        <img src="./img/user 48px.png.png" alt="">
-                    </div>
-                </div>
-                <div class="content-2"></div>
+          <h1>1º Série</h1>
+          <div class="cards">
+            
+            <div class="card">
+              <div class="box">
+             
+              </div>
+            </div>
+            
+            <div class="card">
+              <div class="box">
+                
+              </div>
+            </div>
+            
+                  
+            <div class="card">
+              <div class="box">
+               
+              </div>
+            </div>
+           
+            <div class="card">
+              <div class="box">
+              
+              </div>
+            </div>
+
+            <div class="card">
+              <div class="box">
+              
+              </div>
+            </div>
+
+            <div class="card">
+              <div class="box">
+              
+              </div>
+            </div>
+
+            <div class="card">
+              <div class="box">
+              
+              </div>
+            </div>
+
+            <div class="card">
+              <div class="box">
+              
+              </div>
+            </div>
+
+            <div class="card">
+              <div class="box">
+              
+              </div>
+            </div>
+
+            <div class="card">
+              <div class="box">
+              
+              </div>
+            </div>
+
+            <div class="card">
+              <div class="box">
+              
+              </div>
+            </div>
+
+            <div class="card">
+              <div class="box">
+              
+              </div>
             </div>
         </div>
-    </div>
+      </section>
+            
+        
 </body>
 </html> 
+
+<ul>
+                
+          
